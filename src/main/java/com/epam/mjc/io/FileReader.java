a96fbc02b90d62375708fa22471afa45586393b6
jargr <jaroslav.grablevski@gmail.com>
2023-05-10 19:06:45 +0300

    Profile done

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,45 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+
+        Profile profile = new Profile();
+
+        try (BufferedReader br = new BufferedReader(new java.io.FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] keyValue = line.split(": ");
+
+                switch (keyValue[0]) {
+                    case "Name":
+                        profile.setName(keyValue[1]);
+                        break;
+                    case "Age":
+                        profile.setAge(Integer.parseInt(keyValue[1]));
+                        break;
+                    case "Email":
+                        profile.setEmail(keyValue[1]);
+                        break;
+                    case "Phone":
+                        profile.setPhone(Long.parseLong(keyValue[1]));
+                        break;
+                    default:
+                        break;
+
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return profile;
+
+
     }
 }
